6590042fbfae71e6e29109dbe0ccfc8a715743f1
Mackenzie McIntyre <362574782@GoTVDSB.ca>
2026-02-24 12:27:55 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace If_statements_part_1_lesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
